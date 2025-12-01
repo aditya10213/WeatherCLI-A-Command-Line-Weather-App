e73c24d192af8bf7f332a69e2b012f1bbf12d79b
lax66 <laxmikantamlucky@gmail.com>
2025-12-01 16:49:06 +0000

    feat: add colors

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+from colorama import Fore, Style
+def format_weather(city, weather):
+    return (
+        Fore.CYAN + f"Weather for {city.capitalize()}:\n" + Style.RESET_ALL +
+        f"Temperature: {weather['temperature']}°C\n"
+        f"Windspeed : {weather['windspeed']} km/h\n"
+    )
 def format_weather(city, weather):
     return (
         f"Weather for {city.capitalize()}:\n"
